7c2a9ad8cd72d740de69c7a0b47789f460999b37
guyinxdxn <66008036+guyinxdxn@users.noreply.github.com>
2020-11-23 11:47:04 +0800

    Update 112301.App.java

--- 112301.App.java
+++ 112301.App.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         while (true){
+            
         Scanner scanner =new Scanner(System.in);
         int a=scanner.nextInt();
-        int x=(int)Math.floor(Math.random()*(100));
-        int y=(int)Math.floor(Math.random()*(100));
+            if(a==1){
+        int x=(int)Math.floor(Math.random()*(101));
+        int y=(int)Math.floor(Math.random()*(101));
         System.out.println("R("+x+","+y+")");
+            }
     }}
 }
