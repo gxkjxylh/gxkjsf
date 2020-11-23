52a137a2ec376d74c51a422e5ff2821331141d5a
guyinxdxn <66008036+guyinxdxn@users.noreply.github.com>
2020-11-23 11:58:50 +0800

    Create 112301.App2.java
    定时器返回坐标

---
+++ 112301.App2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+
+
+
+        while (true){
+            Thread.sleep(1000);
+        int x=(int)Math.floor(Math.random()*(101));
+        int y=(int)Math.floor(Math.random()*(101));
+        System.out.println("R("+x+","+y+")");
+    }}
+}
